b4b81c602f81eda873e1ac6aad2439f2ff470fd5
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-14 16:16:21 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -9,3 +10,9 @@ window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption('Tic-Tac-Toe v.1')
 
 # mainloop for running the game
+
+while True:
+    for game in pygame.event.get():
+        if game.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
