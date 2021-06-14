28dd618db3d9a3820dfdfbdb95a6d0c901b3d763
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-14 16:12:37 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+Width = 500
+Height = 500
+
+window = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption('Tic-Tac-Toe v.1')
+
+# mainloop for running the game
