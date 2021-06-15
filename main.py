8dcbe1d6da38230e8bd781680a141c23b351cd6d
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-14 22:01:57 -0500

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 
 Width = 500
 Height = 500
+Line_Width = 10
 
 # colors
 BG_COLOR = (158, 124, 206)
@@ -15,12 +16,16 @@ window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption('Tic-Tac-Toe v.1')
 window.fill(BG_COLOR)
 
-pygame.draw.line(window, BLUE, (10, 10), (250, 250), 10)
+
+# pygame.draw.line(window, BLUE, (10, 10), (250, 250), 10)
 
 
 def lines():
-    pass
+    pygame.draw.line(window, LINE_COLOR, (0, 150), (500, 150), Line_Width)
+    pygame.draw.line(window, LINE_COLOR, (0, 300), (500, 300), Line_Width)
 
+
+lines()
 
 # mainloop for running the game
 while True:
