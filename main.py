2a12d45567870f4cc0af659c45f6e31aee292b43
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-14 22:37:25 -0500

    Initial commit

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ import sys
 
 pygame.init()
 
-Width = 500
-Height = 500
+Width = 450
+Height = 450
 Line_Width = 10
 
 # colors
-BG_COLOR = (158, 124, 206)
-BLUE = (124, 169, 206)
-LINE_COLOR = (127, 197, 113)
+BG_COLOR = (206, 81, 198)
+LINE_COLOR = (202, 158, 200)
 
 window = pygame.display.set_mode((Width, Height))
-pygame.display.set_caption('Tic-Tac-Toe v.1')
+pygame.display.set_caption('TIC TAC TOE v.1')
 window.fill(BG_COLOR)
-
-
-# pygame.draw.line(window, BLUE, (10, 10), (250, 250), 10)
 
 
 def lines():
     pygame.draw.line(window, LINE_COLOR, (0, 150), (500, 150), Line_Width)
     pygame.draw.line(window, LINE_COLOR, (0, 300), (500, 300), Line_Width)
+    pygame.draw.line(window, LINE_COLOR, (150, 0), (150, 500), Line_Width)
+    pygame.draw.line(window, LINE_COLOR, (300, 0), (300, 500), Line_Width)
 
 
 lines()
